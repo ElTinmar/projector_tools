946a08c92d2e7f9f9c486081cdc415e5c85bfd06
ElTinmar <martin.privat@hotmail.fr>
2026-06-04 21:22:04 +0200

    create file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='projector_tools',
+    python_requires='>=3.8',
+    author='Martin Privat',
+    version='0.0.1',
+    packages=['projector_tools'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    description='Remote control video projectors',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "pyserial",
+    ]
+)
